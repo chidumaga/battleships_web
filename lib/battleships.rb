c07f873a6ae07b84bd64f3331f42d71c8ea42b31
Makers Laptop 66 <chidumaga@gmail.com>
2015-05-13 11:45:57 +0100

    added a little css

--- lib/battleships.rb
+++ lib/battleships.rb
@@ -2,6 +2,8 @@ require 'sinatra/base'
 
 class BattleShips < Sinatra::Base
   set :views, Proc.new { File.join(root, "..", "views") }
+  set :public_folder, File.dirname(__FILE__) + '/public'
+
   get '/' do
     'Hello BattleShips!'
     erb :index
